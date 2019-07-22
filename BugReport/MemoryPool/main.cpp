ab96757768e128c6ddbedb8d2f7bd08f34508623
wenyan4work <wenyan4work@gmail.com>
2019-07-22 10:21:26 -0400

    add memorypool bug test

---
+++ BugReport/MemoryPool/main.cpp
@@ -0,0 +1,163 @@
+#include <cassert>
+#include <iostream>
+#include <type_traits>
+#include <vector>
+
+#include "particle_simulator.hpp"
+
+class forceNearEP {
+  public:
+    std::vector<PS::F64vec3> nb; // the number of neighbors found
+    void clear() { nb.clear(); }
+};
+
+/*FullParticle class for FDPS*/
+class SphereFP {
+  public:
+    PS::S32 gid;
+    PS::F64vec3 pos; // Tubule center or protein center
+    PS::F64 RSearch;
+    std::vector<PS::F64vec3> nb; // the number of neighbors found
+
+    PS::F64vec getPos() const { return this->pos; }
+
+    void copyFromForce(const forceNearEP &force) {
+        for (int i = 0; i < force.nb.size(); i++) {
+            nb.push_back(force.nb[i]);
+        }
+    }
+
+    void setPos(const PS::F64vec3 &pos_) { this->pos = pos_; }
+
+    void writeAscii(FILE *fp) const {
+        const double radius = 1;
+        fprintf(fp, "S\t%8d\t%.17g\t%.17g\t%.17g\t%.17g\n", this->gid, radius, this->pos[0], this->pos[1],
+                this->pos[2]);
+    }
+};
+
+class SphereNearForceEP {
+  public:
+    PS::S32 gid;
+    PS::F64vec3 pos;
+    PS::F64 RSearch;
+
+    PS::F64vec getPos() const { return this->pos; }
+
+    void setPos(const PS::F64vec3 &pos) { this->pos = pos; }
+
+    PS::F64 getRSearch() const { return this->RSearch; }
+
+    void copyFromFP(const SphereFP &fp) {
+        gid = fp.gid;
+        pos = fp.pos;
+        RSearch = fp.RSearch;
+    }
+};
+
+class SphereNearForceEPJ {
+  public:
+    PS::S32 gid;
+    PS::F64vec3 pos;
+    PS::F64 RSearch;
+
+    PS::F64vec getPos() const { return this->pos; }
+
+    void setPos(const PS::F64vec3 &pos) { this->pos = pos; }
+
+    PS::F64 getRSearch() const { return this->RSearch; }
+
+    void copyFromFP(const SphereFP &fp) {
+        gid = fp.gid;
+        pos = fp.pos;
+        RSearch = fp.RSearch; // test for EPI/EPJ rsearch differ greatly;
+    }
+};
+
+class CalcNearForceEPIJ {
+  public:
+    // constructor
+    CalcNearForceEPIJ() {}
+
+    // copy constructor
+    CalcNearForceEPIJ(const CalcNearForceEPIJ &obj) {}
+
+    void operator()(const SphereNearForceEP *const ep_i, const PS::S32 Nip, const SphereNearForceEPJ *const ep_j,
+                    const PS::S32 Njp, forceNearEP *const force) {
+
+        for (PS::S32 i = 0; i < Nip; ++i) {
+            force[i].clear();
+            auto &sphereI = ep_i[i];
+            for (PS::S32 j = 0; j < Njp; ++j) {
+                auto &sphereJ = ep_j[j];
+                force[i].nb.push_back(sphereJ.pos);
+            }
+        }
+    }
+};
+
+int main(int argc, char **argv) {
+
+    static_assert(std::is_trivially_destructible<PS::F64vec3>::value);
+    static_assert(std::is_trivially_copyable<PS::F64vec3>::value);
+    static_assert(std::is_trivially_copyable<PS::F64vec2>::value);
+
+    PS::Initialize(argc, argv);
+
+    PS::ParticleSystem<SphereFP> systemSP;
+    systemSP.initialize();
+    PS::DomainInfo dinfo;
+
+    PS::TreeForForceShort<forceNearEP, SphereNearForceEP, SphereNearForceEPJ>::Symmetry treeSymmetry;
+    PS::TreeForForceShort<forceNearEP, SphereNearForceEP, SphereNearForceEPJ>::Scatter treeScatter;
+    CalcNearForceEPIJ calcNearForceFtr;
+
+    systemSP.setNumberOfParticleLocal(2); // 1 particle only
+    // for the same particle, EPI and EPJ should have the same search radius
+    systemSP[0].pos.x = 1.5;
+    systemSP[0].pos.y = 1.5;
+    systemSP[0].pos.z = 1.5;
+    systemSP[0].gid = 0;
+    systemSP[0].RSearch = 0.5; // EPI Rsearch = 1.0, EPJ Rsearch = 1.0
+    systemSP[0].nb.clear();
+    systemSP[1].pos.x = 2.5;
+    systemSP[1].pos.y = 2.5;
+    systemSP[1].pos.z = 2.45;
+    systemSP[1].gid = 1;
+    systemSP[1].RSearch = 0.5; // EPI Rsearch = 10.0, EPJ Rsearch = 10.0
+    systemSP[1].nb.clear();
+
+    dinfo.initialize(0.5);
+    dinfo.setBoundaryCondition(PS::BOUNDARY_CONDITION_PERIODIC_X); // PERIODIC_X
+    dinfo.setPosRootDomain(PS::F64vec3(0, 0, 0), PS::F64vec3(5, 5, 5));
+    systemSP.adjustPositionIntoRootDomain(dinfo);
+    dinfo.decomposeDomainAll(systemSP);
+    systemSP.exchangeParticle(dinfo);
+
+    treeSymmetry.initialize(100);
+    treeScatter.initialize(100);
+
+    std::cout << "n_seg before treeSymmetry" << PS::MemoryPool::getNSegment() << std::endl;
+    treeSymmetry.calcForceAllAndWriteBack(calcNearForceFtr, systemSP, dinfo);
+    std::cout << "n_seg after treeSymmetry" << PS::MemoryPool::getNSegment() << std::endl;
+    int nbfound = systemSP[0].nb.size();
+    std::cout << nbfound << "neighbors found with Symmetry search for Particle 0" << std::endl;
+    for (int i = 0; i < nbfound; i++) {
+        std::cout << systemSP[0].nb[i] << std::endl;
+    }
+
+    systemSP[0].nb.clear();
+    systemSP[1].nb.clear();
+
+    std::cout << "n_seg before treeScatter" << PS::MemoryPool::getNSegment() << std::endl;
+    treeScatter.calcForceAllAndWriteBack(calcNearForceFtr, systemSP, dinfo);
+    std::cout << "n_seg after treeScatter" << PS::MemoryPool::getNSegment() << std::endl;
+    nbfound = systemSP[0].nb.size();
+    std::cout << nbfound << "neighbors found with Scatter search for Particle 0" << std::endl;
+    for (int i = 0; i < nbfound; i++) {
+        std::cout << systemSP[0].nb[i] << std::endl;
+    }
+
+    PS::Finalize();
+    return 0;
+}
